abbffdac5f2eaffc9e029a647887897d0f46099f
fjchen7 <fjchen7@gmail.com>
2025-01-21 14:56:47 +0800

    fetch market data periodically

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use solana_sdk::commitment_config::{CommitmentConfig, CommitmentLevel};
 use solana_sdk::pubkey::Pubkey;
 use anyhow::Error;
 use tokio::time::Interval;
+use log::log;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -43,6 +44,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     .await?;
 
     // let (mut accounts, unsubscriber) = client.account_subscribe(&pool_address, None).await?;
+    // let ws_url =
+    //     "wss://frequent-quiet-field.solana-mainnet.quiknode.pro/1e449a7aa48c912003ee07c443e5514d31cf1395";
+    // let pool_address = "8sLbNZoA1cfnvMJLPfp98ZLAnFSYCFApfJKMbiXNLwxj";
+    // let event_fetcher = swap_event_fetcher::SwapEventFetcher::connect(&ws_url, &pool_address).await.unwrap();
+    // let (mut stream, unsubscriber) = event_fetcher
+    //     .subscribe()
+    //     .await.unwrap();
+    // println!("Subscribed to swap_event event updates");
+    // while let Some(response) = stream.next().await {
+    //     for log in response.value.logs.iter() {
+    //         let event = parse_swap_events(&log).unwrap();
+    //         if let Some(event) = event {
+    //             println!("{:#?}", event);
+    //         }
+    //     }
+    // }
+    // unsubscriber().await;
+    let interval = Duration::from_secs(1);
+    fetch_market_data_and_store_periodically(interval).await;
+    Ok(())
+}
+
+pub async fn fetch_market_data_and_store_periodically(interval: Duration) {
+    let mut interval = tokio::time::interval(interval);
+    let mut latest_swap_event = None;
+    if let Some(latest_swap_event) = latest_swap_event.take() {
+        store_market_data(latest_swap_event);
+    }
     let ws_url =
         "wss://frequent-quiet-field.solana-mainnet.quiknode.pro/1e449a7aa48c912003ee07c443e5514d31cf1395";
     let pool_address = "8sLbNZoA1cfnvMJLPfp98ZLAnFSYCFApfJKMbiXNLwxj";
@@ -51,21 +80,84 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .subscribe()
         .await.unwrap();
     println!("Subscribed to swap_event event updates");
-    while let Some(response) = stream.next().await {
-        for log in response.value.logs.iter() {
-            let event = parse_swap_events(&log).unwrap();
-            if let Some(event) = event {
-                println!("{:#?}", event);
+
+    loop {
+        tokio::select! {
+        v = interval.tick() => {
+            println!("tick next");
+            if let Some(latest_swap_event) = latest_swap_event.take() {
+                let now = time::OffsetDateTime::now_utc();
+                let timestamp_sec = now.unix_timestamp();
+                println!("Current timestamp in milliseconds: {}", timestamp_sec);
+                store_market_data(latest_swap_event);
             }
         }
+        Some(response) = stream.next() => {
+            println!("stream next");
+            let logs = response.value.logs;
+            if let Some(event) = get_latest_swap_event(&logs) {
+                latest_swap_event = Some(event);
+                // store_market_data(latest_swap_event);
+            }
+            // for log in response.value.logs.iter() {
+            //     let event = parse_swap_events(&log).unwrap();
+            //     if let Some(event) = event {
+            //         println!("{:#?}", event);
+            // }
+            // }
+        }
+        }
     }
-    unsubscriber().await;
-    Ok(())
+    // unsubscriber().await?
+}
+
+pub fn store_market_data(event: SwapEvent) {
+    println!("{:#?}", event);
+    // todo!()
 }
 
 
 const PROGRAM_LOG: &str = "Program log: ";
 const PROGRAM_DATA: &str = "Program data: ";
+
+
+// Reference: https://github.com/raydium-io/raydium-clmm/blob/master/client/src/instructions/events_instructions_parse.rs#L181-L183
+pub fn get_latest_swap_event(
+    logs: &[String],
+) -> Option<SwapEvent> {
+    let event = logs.iter().rev().find_map(|log: &String|
+        if let Some(log) = log.strip_prefix(PROGRAM_DATA) {
+            let borsh_bytes = match anchor_lang::__private::base64::decode(log) {
+                Ok(borsh_bytes) => borsh_bytes,
+                _ => {
+                    println!("Could not base64 decode log: {}", log);
+                    return None;
+                }
+            };
+            let mut slice: &[u8] = &borsh_bytes[..];
+            let disc: [u8; 8] = {
+                let mut disc = [0; 8];
+                disc.copy_from_slice(&borsh_bytes[..8]);
+                slice = &slice[8..];
+                disc
+            };
+            if matches!(disc, SwapEvent::DISCRIMINATOR) {
+                match decode_event(&mut slice) {
+                    Ok(event) => {
+                        return Some(event);
+                    }
+                    Err(e) => {
+                        println!("Could not decode event: {}, log {}", e, log);
+                        return None;
+                    }
+                }
+            } else {
+                None
+            }
+        } else { None }
+    );
+    event
+}
 
 
 pub fn parse_swap_events(
@@ -168,6 +260,5 @@ fn decode_event<T: anchor_lang::Event + anchor_lang::AnchorDeserialize>(
     slice: &mut &[u8],
 ) -> anyhow::Result<T> {
     let event: T = anchor_lang::AnchorDeserialize::deserialize(slice)?;
-    // .map_err(|e| ClientError::LogParseError(e.to_string()))?;
     Ok(event)
 }
